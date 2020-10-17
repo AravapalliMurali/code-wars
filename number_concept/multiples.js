fe62fa92df36b695da49b1b82dad7c27a735b7f8
krish77290 <42002445+krish77290@users.noreply.github.com>
2020-10-17 20:51:31 +0530

    adding multiples problems in number_concept folder

---
+++ number_concept/multiples.js
@@ -0,0 +1,20 @@
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Note: If the number is a multiple of both 3 and 5, only count it once. Also, if a number is negative, return 0(for languages that do have them)
+ */
+
+ function multiple(number){
+     let sum = 0 
+     for(let i =0;i<number;i++){
+         //console.log(i)
+         if(i%3 == 0||i%5==0){
+             sum += i
+             console.log(sum)
+         }
+     }
+     return `the sum value:${sum}`
+ }
+ console.log(multiple(10))
